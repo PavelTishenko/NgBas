a2bdc3a4100311c09700498f3e934ec79a12150e
unknown <paveltishen@gmail.com>
2020-01-22 20:31:06 +0200

    new adds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 export interface IPost{
   title: string,
@@ -11,15 +11,26 @@ export interface IPost{
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   posts: IPost[] = [
     {title: 'I want to learn ng components', text:'This is text', id:1},
-    {title: 'Next block', text:'About directives and pipes', id:2},
+    // {title: 'Next block', text:'About directives and pipes', id:2},
   ];
+
+  ngOnInit(){
+    setTimeout(() => {
+      this.posts[0].title = 'Changed'
+    }, 5000)
+  }
 
   updatePosts(post: IPost){
     this.posts.unshift(post);
     // console.log('Post', post);
-
   }
+
+  removePost(id: number){
+    console.log('Id to remove', id);
+    this.posts = this.posts.filter(p => p.id !== id)
+  }
+
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit, Input, ContentChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, ElementRef, OnDestroy, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { IPost } from '../app.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PostComponent implements OnInit {
- @Input() post: IPost;
- @ContentChild('info', {static:true}) infoRef: ElementRef
+export class PostComponent implements OnInit, OnDestroy {
+  @Input() post: IPost;
+  @ContentChild('info', { static: true }) infoRef: ElementRef
+  @Output() onRemove = new EventEmitter<number>();
   constructor() { }
+
+  removePost() {
+    this.onRemove.emit(this.post.id);
+  }
 
   ngOnInit() {
     console.log(this.infoRef.nativeElement);
+  }
+
+  ngOnDestroy(): void {
 
   }
 
