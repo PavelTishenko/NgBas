7fd8c14a48d95a5244f00783de099e377f8a4137
unknown <paveltishen@gmail.com>
2020-01-24 09:31:01 -0800

    encapsulation: ViewEncapsulation.None

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, Input, ContentChild, ElementRef, OnDestroy, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, Input, ContentChild, ElementRef, OnDestroy, Output, EventEmitter, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/core';
 import { IPost } from '../app.component';
+
+
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  // changeDetection: ChangeDetectionStrategy.OnPush, // изменение стратегии определения изменений
+  encapsulation: ViewEncapsulation.None
 })
 export class PostComponent implements OnInit, OnDestroy {
   @Input() post: IPost;
   @ContentChild('info', { static: true }) infoRef: ElementRef
-  @Output() onRemove = new EventEmitter<number>();
+  @Output() onRemove: EventEmitter<number> = new EventEmitter<number>();
   constructor() { }
 
   removePost() {
